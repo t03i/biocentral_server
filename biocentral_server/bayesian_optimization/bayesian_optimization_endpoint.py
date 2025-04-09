e67ec3c25188b09b2d7418697d099695b490c4ff
Shuze Liu <shuze.liu@tum.de>
2025-04-09 18:15:53 +0200

    fix: fultter compatibity

--- biocentral_server/bayesian_optimization/bayesian_optimization_endpoint.py
+++ biocentral_server/bayesian_optimization/bayesian_optimization_endpoint.py
@@ -267,4 +267,4 @@ def model_results():
         )
     with out_file.open("r") as f:
         results_data = yaml.load(f, Loader=yaml.FullLoader)
-        return jsonify(results_data)
+    return {"result": jsonify(results_data)}
